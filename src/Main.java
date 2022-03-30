b832ca9a0060bd004df1255b5ac57694f51c675a
Aleksei Ivanyuk <ivanyuk_aleksei@mail.ru>
2022-03-30 14:52:25 +0300

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -5,111 +5,57 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
 
+        fileDir("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\temp");
+        newFile("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\temp", "temp.txt");
+        fileDir("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\src");
+        fileDir("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\res");
+        fileDir("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\savegames");
+        fileDir("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\src\\main");
+        fileDir("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\src\\test");
+        fileDir("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\res\\drawables");
+        fileDir("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\res\\vectors");
+        fileDir("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\res\\icons");
+        newFile("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\src\\main", "Main.java");
+        newFile("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\src\\main", "Utils.java");
+
+    }
+
+    static void fileDir(String path) {
         StringBuilder Log = new StringBuilder();
-
-// создаем объект File в качестве каталога
-        File dirSrc = new File("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\src");
+        File dirSrc = new File(path);
         if (dirSrc.mkdir())
-            System.out.println("Каталог src создан");
-        Log.append("Каталог src создан");
+            System.out.println("Каталог " + path + " создан");
+        Log.append("Каталог " + path + " создан");
         Log.append('\n');
 
-        File dirRes = new File("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\res");
-        if (dirRes.mkdir())
-            System.out.println("Каталог res создан");
-        Log.append("Каталог res создан");
-        Log.append('\n');
+        String text = Log.toString();
+        try (FileWriter writer = new FileWriter("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\temp\\temp.txt", true)) {
+            writer.write(text);
+            writer.flush();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 
-        File dirSavegames = new File("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\savegames");
-        if (dirSavegames.mkdir())
-            System.out.println("Каталог savegames создан");
-        Log.append("Каталог savegames создан");
-        Log.append('\n');
-
-        File dirTemp = new File("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\temp");
-        if (dirTemp.mkdir())
-            System.out.println("Каталог temp создан");
-        Log.append("Каталог temp создан");
-        Log.append('\n');
-
-        File dirMain = new File("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\src\\main");
-        if (dirMain.mkdir())
-            System.out.println("Каталог main создан");
-        Log.append("Каталог main создан");
-        Log.append('\n');
-
-        File dirTest = new File("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\src\\test");
-        if (dirTest.mkdir())
-            System.out.println("Каталог test создан");
-        Log.append("Каталог test создан");
-        Log.append('\n');
-
-        // создаем объект File для файла, находящегося в каталоге
-        File fileMain = new File("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\src\\main", "Main.java");
+    static void newFile(String pathFile, String nameChild) {
+        StringBuilder Log = new StringBuilder();
+        File fileMain = new File(pathFile, nameChild);
         // создадим новый файл
         try {
             if (fileMain.createNewFile())
-                System.out.println("Файл Main.java был создан");
-            Log.append("Файл Main.java был создан");
+                System.out.println("Файл " + nameChild + " был создан");
+            Log.append("Файл " + nameChild + " был создан");
             Log.append('\n');
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-
-        // создаем объект File для файла, находящегося в каталоге
-        File fileUtils = new File("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\src\\main", "Utils.java");
-        // создадим новый файл
-        try {
-            if (fileUtils.createNewFile())
-                System.out.println("Файл Utils.java был создан");
-            Log.append("Файл Utils.java был создан");
-            Log.append('\n');
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        File dirDrawables = new File("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\res\\drawables");
-        if (dirDrawables.mkdir())
-            System.out.println("Каталог drawables создан");
-        Log.append("Каталог drawables создан");
-        Log.append('\n');
-
-        File dirVectors = new File("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\res\\vectors");
-        if (dirVectors.mkdir())
-            System.out.println("Каталог vectors создан");
-        Log.append("Каталог vectors создан");
-        Log.append('\n');
-
-        File dirIcons = new File("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\res\\icons");
-        if (dirIcons.mkdir())
-            System.out.println("Каталог icons создан");
-        Log.append("Каталог icons создан");
-        Log.append('\n');
-
-        // создаем объект File для файла, находящегося в каталоге
-        File fileTemp = new File("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\temp", "temp.txt");
-        // создадим новый файл
-        try {
-            if (fileTemp.createNewFile())
-                System.out.println("Файл temp.txt был создан");
-            Log.append("Файл temp.txt был создан");
-            Log.append('\n');
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
 
         String text = Log.toString();
-
-        try (FileWriter writer = new FileWriter("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\temp\\temp.txt", false)) {
-
+        try (FileWriter writer = new FileWriter("C:\\Алексей\\Программирование\\JAVA Нетология\\Java Core\\HW Installation\\Games\\temp\\temp.txt", true)) {
             writer.write(text);
-
             writer.flush();
         } catch (IOException ex) {
-
             System.out.println(ex.getMessage());
         }
-
     }
 }
